7ca72e5d5434fff9dbcbc87de25eb3becd7965d8
ViktorHim <vhimlihmail@gmail.com>
2024-02-25 20:29:15 +0300

    add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-const path = require('path');
+const path = require('path');// Подключение модулей
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: "development", // режим разработки
+  entry: path.resolve(__dirname, 'src', 'index.js'), // Входная точка
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build'), // Выходная точка
     filename: '[name].[contenthash].js',
     clean: true
   },
+  plugins: [ //массив плагинов
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new webpack.ProgressPlugin()
+  ] 
 };
